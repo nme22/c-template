6be63c9b0e43f9e1f3115744f3f9b4417ce56d91
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2022-05-23 19:54:55 -0400

    initial commit

---
+++ schemas/lead.js
@@ -0,0 +1,22 @@
+export default {
+   name: 'lead',
+   type: 'document',
+   title: 'Lead',
+   fields: [
+      {
+         name: 'lead',
+         type: 'string',
+         title: 'Lead',
+      },
+      {
+         name: 'email',
+         type: 'string',
+         title: 'Email',
+      },
+      {
+         name: 'phone',
+         type: 'string',
+         title: 'Phone Number',
+      },
+   ],
+};
